33c6342c819bd3ca28dc7941b18d0ed01d123845
manav <manavagr1108@gmail.com>
2021-08-24 18:21:53 +0530

    batting feature
    logic for batting

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 var score = 0;
+var computer_selection;
+var bowling;
+function batting(id)
+{
+    bowling = computer();
+    console.log(bowling)
+    if(bowling == id) 
+    {
+        console.log('out')
+        console.log('batting over your score is:',score)
+    }
+    else{
+        score+=id;
+        console.log('score:', score)
+    }
+}
+
+
+function computer(){
+    computer_selection = Math.floor(Math.random()*7);
+    while(computer_selection==0)
+    {
+        computer_selection = Math.floor(Math.random()*7);
+    }
+    return computer_selection;
+}
